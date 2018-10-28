57e6f24f0202faa9183d8d8bbb8a7023b73afff5
Benoît Gayraud <gayraudbenoit@gmail.com>
2018-10-28 18:17:14 +0100

    dnd clean

--- src/app/dnd-test/dnd-test.component.ts
+++ src/app/dnd-test/dnd-test.component.ts
@@ -20,30 +20,3 @@ export class DndTestComponent implements OnInit {
     moveItemInArray(this.pictureList, event.previousIndex, event.currentIndex);
   }
 }
-// import {Component} from '@angular/core';
-// import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-
-// /**
-//  * @title Drag&Drop sorting
-//  */
-// @Component({
-//   selector: 'app-dnd-test',
-//   templateUrl: './dnd-test.component.html',
-//   styleUrls: ['./dnd-test.component.scss']
-// })
-// export class DndTestComponent {
-//   movies = [
-//     'Episode I - The Phantom Menace',
-//     'Episode II - Attack of the Clones',
-//     'Episode III - Revenge of the Sith',
-//     'Episode IV - A New Hope',
-//     'Episode V - The Empire Strikes Back',
-//     'Episode VI - Return of the Jedi',
-//     'Episode VII - The Force Awakens',
-//     'Episode VIII - The Last Jedi'
-//   ];
-
-//   drop(event: CdkDragDrop<string[]>) {
-//     moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-//   }
-}
